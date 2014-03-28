5b6628b36f05b7e94462d3555cfddf6ecf93113f
jvarley <jakevarley@gmail.com>
2014-03-28 15:08:11 -0400

    refactoring

--- src/staubli_tx60_server.cpp
+++ src/staubli_tx60_server.cpp
@@ -72,7 +72,7 @@ public:
 
 
 template <class ActionType>
-class JointActionManager : public StaubliActionManager
+class StaubliControlActionManager : public StaubliActionManager
 {
 
 protected:
@@ -102,13 +102,6 @@ protected:
         return true;
    }
 
-   /* @pollGoal - Test if the goal is still running
-    *
-    */
-   bool pollGoal(const std::vector<double> &goal_joints)
-   {
-       return(ros::ok() && as_.isActive() && pollRobot(goal_joints));
-   }
 
    virtual void cancelAction()
    {
@@ -160,9 +153,7 @@ protected:
            {
                return false;
            }
-
        }
-
       else {
            abortHard();
            return true;
@@ -171,28 +162,42 @@ protected:
    }
 
 public:
-   JointActionManager(const std::string & actionName, const std::string & actionTopic, double spinRate = 10) : StaubliActionManager (actionName),
-       as_(nh_, actionTopic, boost::bind(&actionCallback, this, _1))
+   JointActionManager(const std::string & actionName, const std::string & actionTopic) : StaubliActionManager (actionName),
+       as_(nh_, actionTopic, boost::bind(&newGoalCallback, this, _1))
    {
    }
 
    virtual void sendGoal() = 0;
 
-   void actionCallback(const ActionType::goal_type_::ConstPtr &goal)
+
+   /*@brief - Callback for recieving a new goal
+    * Should cancel any existing actions that are trying to control the robot
+    * and send the new goal to the robot
+    */
+   void newGoalCallback(const ActionType::goal_type_::ConstPtr &goal)
    {
+       // If there wa sa previous goal of this type, preempt it
+       if(as_.isActive())
+           as_.setPreempted(result_,"Received new goal");
+
        goal_ = *goal;
        if(sendGoal())
        {
            as_.acceptNewGoal(goal_);
-           running = true;
        }
+       // Preempt any other goals and mark this one as running
+       activateAction();
    }
+
+   /* @brief runFeedback - Poll the goal to make sure it is still running and legal, send any feedback that needs sending
+    * to the actions' clients
+    */
 
    void runFeedback()
    {
        if(running)
            if(as_.isActive())
-               running = pollGoal(goal_values_);
+               running = pollRobot(goal_values_);
        else
            {
                //This shouldn't happen
@@ -201,7 +206,7 @@ public:
                    ROS_ERROR(actionName_ + " is active, but not set to running!");
                }
            }
-   }
+   }      
 };
 
 
@@ -697,6 +702,10 @@ int main(int argc, char **argv)
           }
           if(!staubli.IsWorking())
               ROS_ERROR("Staubli not logged in or last request failed");
+
+          //! needs to loop over action server managers and make sure that they are sending feedback if they are running
+          //! for(int asmi = 0; asmi < action_server_managers_.size(); ++asmi)
+          //!     asmi.sendFeedback(/*lastJointValues*/)
 
           loop_rate.sleep();
           ros::spinOnce();
